64474c5c88b8dda5a919e4d6de210b1610f2e2af
Zoya Yazyji <zyazyji@gmail.com>
2023-03-10 11:59:27 +0600

    Update newsListAction.ts

--- src/store/Services/Actions/newsListAction.ts
+++ src/store/Services/Actions/newsListAction.ts
@@ -10,14 +10,14 @@ export const getNewsList = createAsyncThunk("get/newsList", async () => {
     for (let i = 0; i < listId.length; i++) {
       const itemNews = await axios.get(`/item/${listId[i]}.json?print=pretty`);
       const oneNews: INews = {
-        by: itemNews.data.by,
-        id: itemNews.data.id,
-        kids: itemNews.data.kids,
-        score: itemNews.data.score,
-        time: new Date(itemNews.data.time).toDateString(),
-        title: itemNews.data.title,
-        type: itemNews.data.type,
-        url: itemNews.data.url,
+        by: itemNews?.data?.by,
+        id: itemNews?.data?.id,
+        kids: itemNews?.data?.kids,
+        score: itemNews?.data?.score,
+        time: new Date(itemNews?.data?.time).toDateString(),
+        title: itemNews?.data?.title,
+        type: itemNews?.data?.type,
+        url: itemNews?.data?.url,
       };
       listNews.push(oneNews);
     };
