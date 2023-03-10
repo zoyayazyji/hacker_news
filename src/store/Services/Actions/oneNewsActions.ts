bae9a6844dc666a8170d1e800b542a9925707a4f
Zoya Yazyji <zyazyji@gmail.com>
2023-03-10 16:16:32 +0600

    Update oneNewsActions.ts

--- src/store/Services/Actions/oneNewsActions.ts
+++ src/store/Services/Actions/oneNewsActions.ts
@@ -1,9 +1,13 @@
 import axios from '../../../instance';
+import axiosA from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { INews } from '../Slices/newsListSlice';
 
 export const getOneNews = createAsyncThunk("get/oneNews", async (id: number | any) => {
   try {
+    setInterval(async() => {
+    const response = await axiosA.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
+    }, 0);
     const itemNews = await axios.get(`/item/${id}.json?print=pretty`);
     const oneNews: INews = {
       by: itemNews?.data?.by,
